a41a58bc7f7731819a7445c4f37654d4cbe3e2ee
kyalisb <kyalisb19@gmail.com>
2022-07-13 02:12:28 +0100

    BK init commit

---
+++ src/main/java/com/qa/dfespringbootproject/DfeSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dfespringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfeSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfeSpringBootProjectApplication.class, args);
+	}
+
+}
